7d412503131ad3c9ab266059c7e62172a622a979
Aunoy <hasanmahmudul2457@gmail.com>
2022-08-05 17:29:22 +0600

    General-Login-Testing-3

--- Ecommerce-Test-1/src/General_Function_Testing/GeneralLoginTesting3.java
+++ Ecommerce-Test-1/src/General_Function_Testing/GeneralLoginTesting3.java
@@ -4,32 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class GeneralLoginTesting2 {
-	
+public class GeneralLoginTesting3 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = Base.getDriver();
-		
+				
 		///navigating google website
 		//driver.get("http://www.google.com");
-		
+				
 		//navigating google website
 		driver.get("http://127.0.0.1:8000");
-		
-		
+				
+				
 		WebElement element = driver.findElement(By.xpath("//a[contains(@href, 'login')]"));
 		element.click();
-		
-		element = driver.findElement(By.id("phone"));
-		element.sendKeys("1234567");
-		
-		element = driver.findElement(By.id("password"));
-		element.sendKeys("1234");
-		
-		element = driver.findElement(By.id("login"));
-		element.click();
-
+				
+		driver.findElement(By.id("phone")).sendKeys("12345678");
+				
+		driver.findElement(By.id("password")).sendKeys("1234");
+				
+		driver.findElement(By.id("login")).click();
 
 	}
+
 }
